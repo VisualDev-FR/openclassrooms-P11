57f523f4a242134235187676ac4eb77cd7af5390
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2023-04-07 15:53:43 +0200

    create test datas

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,47 @@ import os
 CLUB_DB_TEST_PATH = os.path.join(os.path.dirname(__file__), "data", "clubs.json")
 COMPETITION_BD_TEST_PATH = os.path.join(os.path.dirname(__file__), "data", "competitions.json")
 
+TEST_CLUBS = [
+    {
+        "name": "club_1",
+        "email": "club1@domain.co",
+        "points": "5"
+    },
+    {
+        "name": "club_2",
+        "email": "club2@domain.co",
+        "points": "15"
+    },
+    {
+        "name": "club_3",
+        "email": "club3@domain.co",
+        "points": "20"
+    }
+]
+
+TEST_COMPETITIONS = [
+    {
+        "name": "competition1",
+        "date": "2020-03-27 10:00:00",
+        "numberOfPlaces": "5"
+    },
+    {
+        "name": "competition2",
+        "date": "2025-03-27 10:00:00",
+        "numberOfPlaces": "0"
+    },
+    {
+        "name": "competition2",
+        "date": "2025-03-27 10:00:00",
+        "numberOfPlaces": "0"
+    },
+    {
+        "name": "competition3",
+        "date": "2025-03-27 10:00:00",
+        "numberOfPlaces": "5"
+    },
+]
+
 
 def club_mock(data: dict):
     # overwrite club datas
