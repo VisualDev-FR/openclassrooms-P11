63bddee4cfe31e497ba3c6d9d7c301727be0e0d6
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2023-05-26 10:17:10 +0200

    implement functional test

--- tests/functional/test_ui.py
+++ tests/functional/test_ui.py
@@ -1,8 +1,18 @@
 from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from time import sleep
 
 
-def test_login(driver: Chrome, base_url):
+def test_user_login(driver: Chrome, base_url):
 
     driver.get(base_url + "/")
 
     assert driver.title == "GUDLFT Registration"
+
+    email_field = driver.find_element(By.ID, "email")
+    email_field.send_keys("john@simplylift.co")
+    email_field.submit()
+    sleep(2)
+
+    assert driver.current_url == base_url + "/showSummary"
+    assert "Welcome, john@simplylift.co" in driver.page_source
